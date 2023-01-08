764f1c07834359fc34de40d5eac46e8cd5e0f43f
Jaredmbenitez <61889494+Jaredmbenitez@users.noreply.github.com>
2023-01-08 14:55:54 -0800

    Added more explicit placeholder for Image_url

--- src/components/AddGameForm.tsx
+++ src/components/AddGameForm.tsx
@@ -76,7 +76,7 @@ export default function AddGameForm() {
                 </div>
                 <div>
                     <label className="block text-gray-700 text-sm font-bold mb-2">Image URL</label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Image URL (https://link_to_image)" onChange={(e) => setImageURL(e.target.value)} />
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Image URL (Ex. http://www.boxequalsart.com/wpimages/wp78273398_06.jpg)" onChange={(e) => setImageURL(e.target.value)} />
                 </div>
                 <div className="flex items-center justify-between my-4">
                     <button className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
